d4aed141b901a7fa4d94519282c46b7158c45d97
Chunchun Dan <chunchundan@Chunchuns-MacBook-Air.local>
2020-02-28 21:44:12 +1100

    call state to update latitude

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        );
-        return <div>Latitude: </div>
-    }
+	//javascript method to initialize state
+	//always initialize super method
+	constructor(props) {
+		super(props);
+
+		//this is the only exception we do direct assignment to this state
+		//initially define state
+		this.state = { lat: null };
+
+		window.navigator.geolocation.getCurrentPosition(
+			position => {
+				//always use setState to update state
+				this.setState({ lat: position.coords.latitude })
+			},
+			err => console.log(err)
+		);
+	};
+
+	//react method -> must have
+	render() {
+		return <div>Latitude: {this.state.lat}</div>
+	};
 }
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+	<App />,
+	document.querySelector('#root')
 );
