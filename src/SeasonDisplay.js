46d141779f41f4cc9920566a002346f0cff1524c
Chunchun Dan <chunchundan@Chunchuns-MacBook-Air.local>
2020-03-02 22:10:07 +1100

    Show different texts based on season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 3 && month < 10) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
 const SeasonDisplay = (props) => {
-  return <div>Season Display</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+  return (
+    <h1>
+      {season === 'winter' ? 'Burr, it is chilly!' : 'lets hit the beach!'}
+    </h1>
+  );
 };
 
 export default SeasonDisplay;
