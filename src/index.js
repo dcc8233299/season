03e4139473627af6fdf326e4e1cc340b7e394ccd
Chunchun Dan <chunchundan@Chunchuns-MacBook-Air.local>
2020-03-02 21:23:56 +1100

    do refactoring with lifecycle method

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-	//javascript method to initialize state
-	//always initialize super method
-	constructor(props) {
-		super(props);
+	state = { lat: null, errorMessage: '' };
 
-		//this is the only exception we do direct assignment to this state
-		//initially define state
-		this.state = { lat: null, errorMessage: '' };
-
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			position => {
-				//always use setState to update state
-				this.setState({ lat: position.coords.latitude });
-			},
-			err => {
-				this.setState({ errorMessage: err.message });
-			}
+			position => this.setState({ lat: position.coords.latitude }),
+			err => this.setState({ errorMessage: err.message })
 		);
 	};
 
-	//react method -> must have
 	render() {
 		if (this.state.errorMessage && !this.state.lat) {
 			return <div>Error: {this.state.errorMessage}</div>;
